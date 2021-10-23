5d1829ea567cb9c8e61193737b1ad807cafc8724
Natalia Fernandez <natalia77@live.com>
2021-10-23 15:35:58 -0300

    incorporating marks

--- test_hg_game.py
+++ test_hg_game.py
@@ -2,42 +2,34 @@ from logging import log
 import unittest
 import hg_game
 import numpy as np
+import pytest
+from parameterized import parameterized
 
 class TestHangmanGameGame(unittest.TestCase):
 
     def test_generate_word(self):
         game = hg_game.Game()
-        word = game.generate_word(np.random.randint(len(game.words)))
-
+        word = game.generate_word(1)
         self.assertIsNotNone(word)
         self.assertTrue(word.isalpha())
 
-    def test_risk_word_fail(self):
+    @parameterized.expand([[1, 'materia'],[2, 'facultad'],[0, 'agiles']])
+    def test_risk_word_fail(self, word_number, input_word):
         game = hg_game.Game()
+        game.generate_word(word_number)
+        self.assertTrue(game.risk_word(input_word))
 
-        game.generate_word(1)
-        self.assertTrue(game.risk_word('materia'))
-
-        game.generate_word(2)
-        self.assertTrue(game.risk_word('facultad'))
-        
-        game.generate_word(0)
-        self.assertTrue(game.risk_word('agiles'))
-        
-    def test_risk_word_ok(self):
+    @parameterized.expand([[1, 'facultad'],[2, 'materia'],[0, 'ag1les']])       
+    def test_risk_word_ok(self, word_number, input_word):
         game = hg_game.Game()
-        game.generate_word(1)
+        game.generate_word(word_number)
+        self.assertFalse(game.risk_word(input_word))        
 
-        self.assertFalse(game.risk_word('agiles'))        
-
-    def test_select_level(self):
+    @parameterized.expand([[1, 1, 'lavaplatos'],[2, 1, 'computadora']])  
+    def test_select_level(self, number_word, level, risk_word):
         game = hg_game.Game()
-        game.generate_word(1, 1)        
-        self.assertTrue(game.risk_word('lavaplatos'))
-
-        game2 = hg_game.Game()
-        game2.generate_word(2, 1)        
-        self.assertTrue(game2.risk_word('computadora'))        
+        game.generate_word(number_word, level)        
+        self.assertTrue(game.risk_word(risk_word))     
 
 
 if __name__ == '__main__':
