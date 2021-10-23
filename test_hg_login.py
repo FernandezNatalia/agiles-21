fb8262bbc4e59045dfda3e437ba4dacdf16e9a37
Natalia Fernandez <natalia77@live.com>
2021-10-23 15:40:47 -0300

    incorporating parametrize to login

--- test_hg_login.py
+++ test_hg_login.py
@@ -1,7 +1,7 @@
 from logging import log
 import unittest
 import hg_login
-
+from parameterized import parameterized
 
 class TestHangmanGameLoginPlayer(unittest.TestCase):
 
@@ -15,44 +15,38 @@ class TestHangmanGameLoginPlayer(unittest.TestCase):
         with self.assertRaises(ValueError):
             hg_login.Login('juan123', 'jj123', 'Juan', 'Al0nso')
 
-    def test_login_max_30(self):
+    @parameterized.expand([
+        ['juan123', 'jj123','juan'*6, 'Alonso'],
+        ['juan123', 'jj123','Juan', 'Alonso'*5],
+        ['juan123'*5, 'jj123','Juan', 'Alonso']
+    ])
+    def test_login_max_30(self, user, password, name, lastname):
+        with self.assertRaises(ValueError):
+            hg_login.Login(user, password, name, lastname)
+
+    @parameterized.expand([
+        ['juan123', 'jj123','Ju', 'Alonso'],
+        ['juan123', 'jj123','Juan', 'AA'],
+        ['JA', 'jj123','Juan', 'Alonso']
+    ])
+    def test_login_min_3(self, user, password, name, lastname):
 
         with self.assertRaises(ValueError):
-            hg_login.Login('juan123', 'jj123',
-                               'JuanJuanJuanJuanJuanJuanJuanJuan', 'Alonso')
+            hg_login.Login(user, password, name, lastname)
+
+    @parameterized.expand([
+        ['', 'jj123','juan', 'Alonso'],
+        ['juan123', '','Juan', 'Alonso'],
+        ['juan123', 'jj123','', 'Alonso'],
+        ['juan123', 'jj123','Juan', ''],
+        ['juan123', 'jj123','', ''],
+        ['juan123', '','', ''],
+        ['', '','', '']
+    ])
+    def test_input_null(self, user, password, name, lastname):
 
         with self.assertRaises(ValueError):
-            hg_login.Login('juan123', 'jj123', 'Juan',
-                               'AlonsoAlonsoAlonsoAlonsoAlonsoAlonso')
-
-        with self.assertRaises(ValueError):
-            hg_login.Login(
-                'juan123juan123juan123juan123juan123', 'jj123', 'Juan', 'Alonso')
-
-    def test_login_min_3(self):
-
-        with self.assertRaises(ValueError):
-            hg_login.Login('juan123', 'jj123', 'Ju', 'Alonso')
-
-        with self.assertRaises(ValueError):
-            hg_login.Login('juan123', 'jj123', 'Juan', 'AA')
-
-        with self.assertRaises(ValueError):
-            hg_login.Login('23', 'jj123', 'Juan', 'Alonso')
-
-    def test_input_null(self):
-
-        with self.assertRaises(ValueError):
-            hg_login.Login('', 'jj123', 'Juan', 'Alonso')
-
-        with self.assertRaises(ValueError):
-            hg_login.Login('juan123', '', 'Juan', 'Alonso')
-
-        with self.assertRaises(ValueError):
-            hg_login.Login('juan123', 'jj123', '', 'Alonso')
-
-        with self.assertRaises(ValueError):
-            hg_login.Login('juan123', 'jj123', 'Juan', '')
+            hg_login.Login(user, password, name, lastname)
 
 
 if __name__ == '__main__':
