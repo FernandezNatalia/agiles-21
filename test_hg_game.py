634ba55e8569a42f02a487127b8dd1a442edd599
Natalia Fernandez <natalia77@live.com>
2021-10-23 16:09:07 -0300

    adding pytest run

--- test_hg_game.py
+++ test_hg_game.py
@@ -1,35 +1,29 @@
-class TestHangmanGameGame(unittest.TestCase):
+import hg_game
+import numpy as np
+from parameterized import parameterized
 
-    def test_generate_word(self):
-        game = hg_game.Game()
-        word = game.generate_word(1)
-        self.assertIsNotNone(word)
-        self.assertTrue(word.isalpha())
+def test_generate_word():
+    game = hg_game.Game()
+    word = game.generate_word(np.random.randint(len(game.words)))
+    assert word is not None
+    assert word.isalpha()
 
-    @parameterized.expand([[1, 'materia'],[2, 'facultad'],[0, 'agiles']])
-    def test_risk_word_fail(self, word_number, input_word):
-        game = hg_game.Game()
-        game.generate_word(word_number)
-        self.assertTrue(game.risk_word(input_word))
+@parameterized.expand([[1, 'materia'],[2, 'facultad'],[0, 'agiles']])
+def test_risk_word_fail(word_number, input_word):
+    game = hg_game.Game()
+    game.generate_word(word_number)
+    assert game.risk_word(input_word)
 
-    @parameterized.expand([[1, 'facultad'],[2, 'materia'],[0, 'ag1les']])       
-    def test_risk_word_ok(self, word_number, input_word):
-        game = hg_game.Game()
-        game.generate_word(word_number)
-        self.assertFalse(game.risk_word(input_word))        
+@parameterized.expand([[1, 'facultad'],[2, 'materia'],[0, 'ag1les']])       
+def test_risk_word_ok(word_number, input_word):
+    game = hg_game.Game()
+    game.generate_word(word_number)
+    assert not game.risk_word(input_word)     
 
-    @parameterized.expand([[1, 1, 'lavaplatos'],[2, 1, 'computadora']])  
-    def test_select_level(self, number_word, level, risk_word):
-        game = hg_game.Game()
-        game.generate_word(number_word, level)        
-        self.assertTrue(game.risk_word(risk_word))     
+@parameterized.expand([[1, 1, 'lavaplatos'],[2, 1, 'computadora']])  
+def test_select_level(number_word, level, risk_word):
+    game = hg_game.Game()
+    game.generate_word(number_word, level)        
+    assert game.risk_word(risk_word)   
 
-
-if __name__ == '__main__':
-    from logging import log
-    import unittest
-    import hg_game
-    import pytest
-    from parameterized import parameterized
-    unittest.main()
 
--- test_hg_player.py
+++ test_hg_player.py
@@ -1,29 +1,21 @@
-from logging import log
-import unittest
 import hg_player
 
-class TestHangmanGameLoginPlayer(unittest.TestCase):
+player = hg_player.Player('juan123', 'jj123', 'Juan', 'Alonso')
 
-    def setUp(self):
-        self.player = hg_player.Player('juan123', 'jj123', 'Juan', 'Alonso')
+def test_firstname():
+    assert player.first_name == 'Juan'
 
-    def test_firstname(self):
-        self.assertEqual(self.player.first_name, 'Juan')
+def test_lastname():
+    assert player.last_name == 'Alonso'
 
-    def test_lastname(self):
-        self.assertEqual(self.player.last_name, 'Alonso')
+def test_username():
+    assert player.username == 'juan123'
 
-    def test_username(self):
-        self.assertEqual(self.player.username, 'juan123')
+def test_pass():
+    assert player.password == 'jj123'
 
-    def test_pass(self):
-        self.assertEqual(self.player.password, 'jj123')
+def test_lifes():
+    assert player.lifes == 3
 
-    def test_lifes(self):
-        self.assertEqual(self.player.lifes, 3)
-
-    def test_fullname(self):
-        self.assertEqual(self.player.fullname, 'Juan Alonso')   
-
-if __name__ == '__main__':
-    unittest.main()
+def test_fullname():
+    assert player.fullname == 'Juan Alonso'  
