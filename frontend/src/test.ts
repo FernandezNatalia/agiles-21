af08c45bd0232b9f1489954d5cb2428434516847
FernandezNatalia <natalia77@live.com>
2022-01-23 15:15:52 -0300

    foreach problem with sonarcloud

--- frontend/src/test.ts
+++ frontend/src/test.ts
@@ -23,4 +23,4 @@ getTestBed().initTestEnvironment(
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
-context.keys().map(context);
+const m = context.keys().map(context);
