9bdcd6190d0131487e420d6e9d161bf46c201c78
FernandezNatalia <natalia77@live.com>
2022-01-23 15:10:24 -0300

    flask with cstf

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect()
+csrf.init_app(app) # Compliant
 
 if __name__=='__main__':
     app.run(debug = True, port = 4000)
